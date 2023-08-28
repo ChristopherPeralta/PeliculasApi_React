ec6a1ab1ea08015b7c0c4f82817cb21ff4433934
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-08-28 00:18:30 -0500

    cssMovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,15 @@
+import "../components/MovieCard.css"
+
 export function MovieCard({ movie }) {
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
 
     return (
-        <li>
+        <li className="movieCard">
             <img width={230}
             height={345}
             src={imageUrl}
-            alt={movie.title}/>
+            alt={movie.title}
+            className="movieImage"/>
             <div>{movie.title}</div>
         </li>
         )
