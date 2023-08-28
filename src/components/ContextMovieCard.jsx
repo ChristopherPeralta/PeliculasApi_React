811e5e8209c303500b4f22fd74f90326bb292b40
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-08-28 00:08:57 -0500

    card

--- src/components/ContextMovieCard.jsx
+++ src/components/ContextMovieCard.jsx
@@ -1,4 +1,22 @@
-export function ContextMovieCard(){
-    return (<h1>Context Movie Card</h1>)
-}
+import { useEffect, useState } from "react";
+import { get } from "../data/dataApi";
+import { MovieCard } from "./MovieCard";
 
+export function ContextMovieCard() {
+  const [movies, SetMovies] = useState([]);
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      SetMovies(data.results);
+        console.log(data)
+
+    });
+  }, []);
+
+  return (
+    <ul>
+        {movies.map((movie)=>(
+            <MovieCard movie={movie}/>
+        ))}
+    </ul>
+  );
+}
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,14 @@
-export function MovieCard(){
-    return (<h1>Movie Card</h1>)
+export function MovieCard({ movie }) {
+    const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+
+    return (
+        <li>
+            <img width={230}
+            height={345}
+            src={imageUrl}
+            alt={movie.title}/>
+            <div>{movie.title}</div>
+        </li>
+        )
 }
+
